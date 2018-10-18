7016b23538afd8d8162216b3b7dbe1069f79eb00
Dominic Burkart <1dominicb@gmail.com>
2018-10-17 23:48:31 -0400

    add infractions check

--- march311.py
+++ march311.py
@@ -11,14 +11,73 @@ binned311 = pd.read_csv("311_Service_Requests_from_2010_to_Present.csv")
 
 number_calls = [] # number of 311 calls on the building targeted by each raid.
 within_month = [] # number of 311 calls on the building targeted by each raid in the preceding 30 days.
+within_month_valid = []
 within_year = [] # number of 311 calls on the building targeted by each raid in the preceding 365 days.
+within_year_valid = []
 preceding_set = []
 
 MONTH = datetime.timedelta(days = 30)
 YEAR = datetime.timedelta(days = 365)
 
-i = 0
+INFRACTIONS = set(
+    "Noise - Commercial",
+    "Noise - Residential",
+    "Noise - Street/Sidewalk",
+    "Food Establishment",
+    "Noise Survey",
+    "Drinking",
+    "Building/Use",
+    "Consumer Complaint",
+    "Dirty Conditions",
+    "Noise",
+    "UNSANITARY CONDITION",
+    "Graffiti",
+    "Rodent",
+    "Smoking",
+    "PAINT/PLASTER",
+    "PAINT - PLASTER",
+    "DOOR/WINDOW",
+    "Other Enforcement",
+    "GENERAL",
+    "Blocked Driveway",
+    "FLOORING/STAIRS",
+    "Sanitation Condition",
+    "Special Projects Inspection Team (SPIT)",
+    "Food Poisoning",
+    "Electrical",
+    "WATER LEAK",
+    "Non-Emergency Police Matter",
+    "Plumbing",
+    "SAFETY",
+    "Indoor Air Quality",
+    "Non-Residential Heat",
+    "Elevator",
+    "Animal Abuse",
+    "Industrial Waste",
+    "Water Quality",
+    "Lead",
+    "Hazardous Materials",
+    "Asbestos",
+    "Vending",
+    "Mold",
+    "Investigations and Discipline (IAD)",
+    "Drinking Water",
+    "Boilers",
+    "Noise - House of Worship",
+    "Standing Water",
+    "Scaffold Safety",
+    "OUTSIDE BUILDING",
+    "Root/Sewer/Sidewalk Condition",
+    "School Maintenance",
+    "Traffic",
+    "Noise - Park",
+    "Beach/Pool/Sauna Complaint",
+    "Drug Activity",
+    "Indoor Sewage",
+    "Cranes and Derricks"
+)
 
+i = 0
 old = None
 
 for index, raid in march_with_lat.iterrows():
@@ -41,7 +100,9 @@ for index, raid in march_with_lat.iterrows():
             lambda created: True if parser.parse(created) < raid_time and raid_time - parser.parse(created) < YEAR else False
         )
         preceding_month = linked_311s[linked_311s["preceding_month"] == True]
+        preceding_month_valid = preceding_month[preceding_month['Complaint Type'].isin(INFRACTIONS)]
         preceding_year = linked_311s[linked_311s["preceding_year"] == True]
+        preceding_year_valid = preceding_year[preceding_year['Complaint Type'].isin(INFRACTIONS)]
         within_month.append(preceding_month.shape[0])
         within_year.append(preceding_year.shape[0])
         preceding_set.extend(preceding_year['Complaint Type'].values)
@@ -49,8 +110,12 @@ for index, raid in march_with_lat.iterrows():
         created = parser.parse(linked_311s['Created Date'])
         if created < raid_time and raid_time - created < MONTH:
             within_month.append(1)
+            if INFRACTIONS.contains(linked_311s['Complaint Type']):
+                within_month_valid.append(1)
         if created < raid_time and raid_time - created < YEAR:
             within_year.append(1)
+            if INFRACTIONS.contains(linked_311s['Complaint Type']):
+                within_year_valid.append(1)
             preceding_set.append(linked_311s['Complaint Type'])
     i += 1
 
@@ -61,7 +126,9 @@ march_with_lat.to_csv("march_with_raid_links.csv",index=False)
 print("proportion ever 311'd: ", str(float(len([v for v in number_calls if v > 0])) / march_with_lat.shape[0]))
 print("proportion preceded by 311 (30 days): "+str(float(len([v for v in within_month if v > 0])) / march_with_lat.shape[0]))
 print("proportion preceded by 311 (365 days): "+str(float(len([v for v in within_year if v > 0])) / march_with_lat.shape[0]))
+print("valid proportion preceded by 311 (30 days): "+str(float(len([v for v in within_month_valid if v > 0])) / march_with_lat.shape[0]))
+print("valid proportion preceded by 311 (365 days): "+str(float(len([v for v in within_year_valid if v > 0])) / march_with_lat.shape[0]))
+
 
 preceding_set = Counter(preceding_set)
 print(preceding_set)
-pd.DataFrame.from_records(preceding_set).to_csv("preceding.csv", index=False)
